5f2385e97e98c05d548108d085f269d5416c2b3a
laizuakram1 <laizuakram98@gmail.com>
2022-06-20 09:13:32 +0600

    create get all api

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+//middle ware3
+app.use(cors())
+app.use(express.json())
+
+
+//connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xc0o9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+
+    await client.connect();
+    const yarnCollection = client.db("AllYarns").collection("yarn");
+
+
+    //get all yarns
+    app.get('/yarns', async(req, res) =>{
+        const query ={};
+        const result = await yarnCollection.find(query).toArray();
+
+        res.send(result);
+    })
+    
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
